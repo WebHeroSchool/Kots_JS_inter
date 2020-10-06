70d0c3097f76082d681ad903852bcf6ee02553f2
Коционова Анна <kotsionova@yandex.ru>
2020-10-06 17:12:57 +0300

    Добавлена карточная раскладка

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const easyLvl = document.getElementById('easyLvl');
 const midLvl = document.getElementById('midLvl');
 const heavyLvl = document.getElementById('heavyLvl');
 const btn = document.getElementById('confirmBtn');
+const menu = document.getElementById('menu');
+const game = document.getElementById('game');
 let lvl = 0;
 
 easyLvl.addEventListener('click', () => {
@@ -26,8 +28,27 @@ heavyLvl.addEventListener('click', () => {
   heavyCheck.classList.add('selected');
 });
 
-btn.addEventListener('click', startGame(lvl));
+btn.addEventListener('click', startGame(event));
 
-function startGame(level) {
-
+function startGame() {
+  return function() {
+    menu.classList.add('displayNone');
+    game.classList.remove('displayNone');
+    for (i = 0; i <= lvl; i++) {
+      console.log(i);
+      const newLine = document.createElement('div');
+      newLine.className = 'cardLine';
+      newLine.innerHTML = `<img src="img/card.png" alt="Карта" class="game__card">
+      <img src="img/card.png" alt="Карта" class="game__card">
+      <img src="img/card.png" alt="Карта" class="game__card">`;
+      game.append(newLine);
+    }
+    if (lvl === 0) {
+      game.classList.add('game_easy');
+    } else if (lvl === 1) {
+      game.classList.add('game_mid');
+    } else {
+      game.classList.add('game_heavy');
+    }
+  }
 }
